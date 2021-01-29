bb826b7177906d6196e4557f11b3c7b2b567daff
TeraNaidja <phildegroot5@gmail.com>
2021-01-29 11:03:52 +0100

    Changed KPIs to contain the month that they were calculated for in the request. Fixed MEL so it works with the new API design. MEL now uses tasks instead of threads to initialize things, and is better at handling all the concurrency

--- api/v1/class.kpi.php
+++ api/v1/class.kpi.php
@@ -11,42 +11,39 @@
 		 * @apiGroup KPI
 		 * @api {GET} /kpi/post Post
 		 * @apiParam {string} name name of the KPI
+		 * @apiParam {int} month Month that this KPI applies to.
 		 * @apiParam {int} value the value of this months kpi
 		 * @apiParam {string} type the type of KPI (ECOLOGY, ENERGY, SHIPPING)
 		 * @apiParam {string} unit the measurement unit of this KPI
 		 * @apiParam {int} country (OPTIONAL) id of the country that this belongs to. Not filling this in will default it to all countries
 		 * @apiDescription Add a new kpi value to the database
 		 */
-		public function Post(string $name, int $value, string $type, string $unit, int $country = -1)
+		public function Post(string $name, int $month, int $value, string $type, string $unit, int $country = -1)
 		{
-			//not passing the month from the simulations means the server is more authorative, maybe the month should be passed by the sims? 
-			//This would allow a desync between a simulation and the server
-
-			return $this->PostKPI($name, $value, $type, $unit, $country);
+			return $this->PostKPI($name, $month, $value, $type, $unit, $country);
 		}
 
 		/**
 		 * @apiGroup KPI
-		 * @api {GET} /kpi/post Post
-		 * @apiParam {string[]} kpiValues Json encoded string of the kpi values that we need to insert on the database. Input format should be [{"name":(string kpiName),"value":(int kpiValue),"type":(string kpiType),"unit":(string kpiUnit),"country":(int countryId or null)}] 
+		 * @api {GET} /kpi/BatchPost BatchPost
+		 * @apiParam {array} kpiValues Input format should be [{"name":(string kpiName),"month": (int month), "value":(int kpiValue),"type":(string kpiType),"unit":(string kpiUnit),"country":(int countryId or null)}] 
 		 * @apiDescription Add a new kpi value to the database
 		 */
-		public function BatchPost($kpiValues)
+		public function BatchPost(array $kpiValues)
 		{
-			$kpiValues = json_decode($kpiValues);
 			foreach($kpiValues as $value)
 			{
-				$this->PostKPI($value->name, $value->value, $value->type, $value->unit, $value->country);
+				$this->PostKPI($value["name"], $value["month"], $value["value"], $value["type"], $value["unit"], $value["country"]);
 			}
 		}
 
-		private function PostKPI($kpiName, $kpiValue, $kpiType, $kpiUnit, $kpiCountry = -1)
+		private function PostKPI(string $kpiName, int $kpiMonth, int $kpiValue, string $kpiType, string $kpiUnit, int $kpiCountry = -1)
 		{
 			$value = floatval(str_replace(",", ".", $kpiValue));
 			return $this->query("INSERT INTO kpi (kpi_name, kpi_value, kpi_month, kpi_type, kpi_lastupdate, kpi_unit, kpi_country_id) 
-					VALUES (?, ?, (SELECT game_currentmonth FROM game WHERE game_id=1), ?, ?, ?, ?)
+					VALUES (?, ?, ?, ?, ?, ?, ?)
 					ON DUPLICATE KEY UPDATE kpi_value = ?, kpi_lastupdate = ?", 
-				array($kpiName, $value, $kpiType, microtime(true), $kpiUnit, $kpiCountry, $value, microtime(true)), true
+				array($kpiName, $value, $kpiMonth, $kpiType, microtime(true), $kpiUnit, $kpiCountry, $value, microtime(true)), true
 			);
 		}
 
