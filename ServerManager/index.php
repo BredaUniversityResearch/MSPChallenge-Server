64fb1f01acf1950746db19331b713952fb8679e6
Harald Warmelink <warmelink.h@buas.nl>
2021-04-21 14:49:26 +0200

    fixes bug for new server installations

--- ServerManager/index.php
+++ ServerManager/index.php
@@ -2,15 +2,14 @@
 require_once 'init.php';
 $servermanager = ServerManager::getInstance();
 
-// run any outstanding database migrations
-$db = DB::getInstance();
-$db->dbase_migrate();
-
 if(isset($user) && $user->isLoggedIn()){
   if ($servermanager->freshinstall()) {
+    // new installation, redirect to set up ServerManager database
     Redirect::to($servermanager->GetServerManagerFolder().'install/install.php');
   }
   else {
+    // run any outstanding database migrations
+    $db = DB::getInstance()->dbase_migrate();
     Redirect::to($servermanager->GetServerManagerFolder().'manager.php');
   }
 }
