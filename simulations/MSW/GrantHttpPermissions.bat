78b633b85787b195dfaf994346611a360d930964
Marin <hekman.m@buas.nl>
2022-03-15 09:10:56 +0100

    removed adding and removal of MSP Websocket server firewall rules from MSWs Grant/RevokeHttpPermissions batch files. They have been moved to /wsserver-win-service.bat

--- simulations/MSW/GrantHttpPermissions.bat
+++ simulations/MSW/GrantHttpPermissions.bat
@@ -1,3 +1,2 @@
 netsh http add urlacl url=http://+:45000/Watchdog/ user="%USERDOMAIN%\%USERNAME%"
-netsh advfirewall firewall add rule name="MSP Websocket server" dir=in action=allow protocol=TCP localport=45001
 pause
--- simulations/MSW/RevokeHttpPermissions.bat
+++ simulations/MSW/RevokeHttpPermissions.bat
@@ -1,3 +1,2 @@
 netsh http delete urlacl url=http://+:45000/Watchdog/
-netsh advfirewall firewall delete rule name="MSP Websocket server"
 pause
