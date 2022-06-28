9dbb339ac7db53b3414f39eaa44017d5e5c903eb
Warmelink <warmelink.h@buas.nl>
2022-06-28 11:58:34 +0200

    MSP-3953 turns type definition in source geodata into string again to support multitypes

--- api/v1/Store.php
+++ api/v1/Store.php
@@ -327,7 +327,7 @@ class Store extends Base
     private function moveDataFromArray(
         array $layerMetaData,
         array &$featureProperties,
-        int &$type,
+        string &$type,
         ?int &$mspId,
         ?int &$countryId
     ): void {
@@ -354,10 +354,9 @@ class Store extends Base
             if ($type == -1) {
                 $type = $typeOther ?? 0;
             }
-        } elseif (isset($featureProperties['type'])
-            && is_numeric($featureProperties['type'])
-        ) {
-            $type = intval($featureProperties['type']);
+        } elseif (isset($featureProperties['type']))
+        {
+            $type = $featureProperties['type'];
             unset($featureProperties['type']);
         }
 
@@ -386,7 +385,7 @@ class Store extends Base
         int $layerId,
         string $jsonData,
         ?int $countryId,
-        int $type,
+        string $type,
         ?int $mspId,
         string $filename
     ): void {
@@ -542,7 +541,7 @@ class Store extends Base
         string $geometry,
         string $data,
         ?int $countryId,
-        int $type,
+        string $type,
         ?int $mspId,
         int $subtractive = 0,
         string $layerName = ''
