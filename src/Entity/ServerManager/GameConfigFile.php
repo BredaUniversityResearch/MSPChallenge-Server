512cc2673d761726d014bafd1e80a1928df5b97e
Marin <hekman.m@buas.nl>
2022-11-16 21:53:47 +0100

    forgot to commit new files

---
+++ src/Entity/ServerManager/GameConfigFile.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity\ServerManager;
+
+use App\Repository\ServerManager\GameConfigFileRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: 'game_config_files')]
+#[ORM\Entity(repositoryClass: GameConfigFileRepository::class)]
+class GameConfigFile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * @note no whitespaces and no special characters please and without file extension (.json)
+     */
+    #[ORM\Column(length: 45)]
+    private ?string $filename = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
