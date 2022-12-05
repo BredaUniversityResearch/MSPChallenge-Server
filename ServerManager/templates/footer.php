74216093739faa647373744c3b483b2e599d4448
Harald Warmelink <warmelink.h@buas.nl>
2022-12-05 17:53:21 +0100

    MSP-4205 implements new graphical style, to be refactored and tweaked once we start using Twig and Webpack Encore (after RC1)

--- ServerManager/templates/footer.php
+++ ServerManager/templates/footer.php
@@ -1,18 +1,29 @@
 <footer style="text-align: center; padding-top: 15px;">
 
-  <div>
-    MSP Challenge Server version <?php echo ServerManager::getInstance()->GetCurrentVersion(); ?>
-  </div>
-  <div>
-    Server Address: 
-    <?php 
-    $address = ServerManager::getInstance()->GetTranslatedServerURL();
-    if ($address == "localhost") {
-      $address .= "<br/>Translated automatically to ".gethostbyname(gethostname());
-    }
-    echo $address;
-    ?>
-  </div>
+
+    <div id="footer">
+        MSP Challenge is a community-based, open source and not for profit initiative, since 2011.
+    </div>
+    <div id="footer-extra">
+        <img src="images/EU_flag_yellow_high.jpg" style="width: 40px;"/>
+        Co-funded by the European Union.
+    </div>
+    <div>
+        <p></p>
+    </div>
+    <div>
+        MSP Challenge Server version <?php echo ServerManager::getInstance()->GetCurrentVersion(); ?>
+    </div>
+    <div>
+        Server Address:
+        <?php
+        $address = ServerManager::getInstance()->GetTranslatedServerURL();
+        if ($address == "localhost") {
+            $address .= "<br/>Translated automatically to ".gethostbyname(gethostname());
+        }
+        echo $address;
+        ?>
+    </div>
 
 </footer>
 </body>
