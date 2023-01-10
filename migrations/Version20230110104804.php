68ce7182ad325779f093897315932b7bb543ceb5
Harald Warmelink <warmelink.h@buas.nl>
2023-01-10 14:21:32 +0100

    MSP-4194 code sniffer fix

--- migrations/Version20230110104804.php
+++ migrations/Version20230110104804.php
@@ -20,14 +20,26 @@ final class Version20230110104804 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE game_saves CHANGE save_timestamp save_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL');
-        $this->addSql('ALTER TABLE users CHANGE token token LONGTEXT NOT NULL, CHANGE refresh_token refresh_token LONGTEXT NOT NULL, CHANGE refresh_token_expiration refresh_token_expiration DATETIME NOT NULL');
+        $this->addSql(<<<'SQL'
+ALTER TABLE game_saves CHANGE save_timestamp save_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+SQL);
+        $this->addSql(<<<'SQL'
+ALTER TABLE users CHANGE token token LONGTEXT NOT NULL, 
+    CHANGE refresh_token refresh_token LONGTEXT NOT NULL, 
+    CHANGE refresh_token_expiration refresh_token_expiration DATETIME NOT NULL
+SQL);
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE game_saves CHANGE save_timestamp save_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
-        $this->addSql('ALTER TABLE users CHANGE token token TEXT NOT NULL, CHANGE refresh_token refresh_token TEXT DEFAULT NULL, CHANGE refresh_token_expiration refresh_token_expiration DATETIME DEFAULT NULL');
+        $this->addSql(<<<'SQL'
+ALTER TABLE game_saves CHANGE save_timestamp save_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+SQL);
+        $this->addSql(<<<'SQL'
+ALTER TABLE users CHANGE token token TEXT NOT NULL, 
+    CHANGE refresh_token refresh_token TEXT DEFAULT NULL, 
+    CHANGE refresh_token_expiration refresh_token_expiration DATETIME DEFAULT NULL
+SQL);
     }
 }
