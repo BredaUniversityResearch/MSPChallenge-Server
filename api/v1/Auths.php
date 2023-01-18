f96f660d03f7965ee929030925f90c058ab6e5ce
Harald Warmelink <warmelink.h@buas.nl>
2023-01-18 14:00:11 +0100

    MSP-4298 removes unnecessary code

--- api/v1/Auths.php
+++ api/v1/Auths.php
@@ -4,20 +4,6 @@ namespace App\Domain\API\v1;
 
 abstract class Auths extends Base
 {
-    // delete this after initial development!
-    private const ALLOWED = array(
-        ["getName", Security::ACCESS_LEVEL_FLAG_NONE],
-        ["authenticate", Security::ACCESS_LEVEL_FLAG_NONE],
-        ["checkuser", Security::ACCESS_LEVEL_FLAG_NONE],
-        ["authorize", Security::ACCESS_LEVEL_FLAG_NONE],
-    );
-    // because authconn will talk to the auth providers only
-
-    public function __construct(string $method = '')
-    {
-        parent::__construct($method, self::ALLOWED);
-    }
-
     abstract public function getName(): string;
     abstract public function authenticate(string $username, string $password): bool;
 
