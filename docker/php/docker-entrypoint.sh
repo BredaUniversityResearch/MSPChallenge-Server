5431438b7f46c42c12e518b81fc71c9a5005a89e
Marin <hekman.m@buas.nl>
2023-03-27 22:35:24 +0200

    also do composer install on prod, there seems to be a dependency on dbal:run call...

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -27,9 +27,9 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		rm -Rf tmp/
 	fi
 
-	if [ "$APP_ENV" != 'prod' ]; then
+	#if [ "$APP_ENV" != 'prod' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
-	fi
+	#fi
 
 	if grep -q ^DATABASE_URL= .env; then
 		# After the installation, the following block can be deleted
