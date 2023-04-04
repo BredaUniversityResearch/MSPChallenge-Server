774163489ecafa4d07c36c6dda31ecb137f448a8
Marin <hekman.m@buas.nl>
2023-04-04 17:53:01 +0200

    handle deprecated but still supported headers

--- src/Domain/WsServer/ClientHeaderKeys.php
+++ src/Domain/WsServer/ClientHeaderKeys.php
@@ -6,4 +6,10 @@ interface ClientHeaderKeys
 {
     const HEADER_KEY_GAME_SESSION_ID = 'Game-Session-Id';
     const HEADER_KEY_MSP_API_TOKEN = 'Msp-Api-Token';
+
+    // deprecated but still supported
+    const HEADER_KEYS_DEPRECATED = [
+        self::HEADER_KEY_GAME_SESSION_ID => 'GameSessionId',
+        self::HEADER_KEY_MSP_API_TOKEN => 'MSPAPIToken'
+    ];
 }
--- src/Domain/WsServer/WsServer.php
+++ src/Domain/WsServer/WsServer.php
@@ -140,12 +140,24 @@ class WsServer extends EventDispatcher implements
         );
 
         if (!array_key_exists(self::HEADER_KEY_GAME_SESSION_ID, $headers) ||
-            !array_key_exists(self::HEADER_KEY_MSP_API_TOKEN, $headers)) {
+            !array_key_exists(self::HEADER_KEY_MSP_API_TOKEN, $headers) ||
+            // encountered some deprecated headers still supported
+            count(array_intersect_key($headers, array_flip(self::HEADER_KEYS_DEPRECATED))) == 0) {
             // required headers are not there, do not allow connection
             wdo('required headers are not there, do not allow connection');
             $conn->close();
             return;
         }
+
+        // fix deprecated headers
+        foreach (self::HEADER_KEYS_DEPRECATED as $newHeaderName => $deprecatedHeaderName) {
+            if (!array_key_exists($deprecatedHeaderName, $headers)) {
+                continue;
+            }
+            $headers[$newHeaderName] = $headers[$deprecatedHeaderName];
+            unset($headers[$deprecatedHeaderName]);
+        }
+
         $gameSessionId = $headers[self::HEADER_KEY_GAME_SESSION_ID];
         if (null != $this->gameSessionIdFilter && $this->gameSessionIdFilter != $gameSessionId) {
             // do not connect this client, client is from another game session
