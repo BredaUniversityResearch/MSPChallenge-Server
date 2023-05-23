1e878166bddf473438ef59154e4164c384564fb1
Marin <89514754+marinhekman@users.noreply.github.com>
2023-05-23 10:50:14 +0000

    added a "start simulations" button to the Symfony web profiler bar in the MSP section (#141)

--- src/DataCollector/MSPDataCollector.php
+++ src/DataCollector/MSPDataCollector.php
@@ -2,10 +2,15 @@
 
 namespace App\DataCollector;
 
+use App\Domain\API\APIHelper;
+use App\Domain\API\v1\Game;
+use App\Domain\Services\ConnectionManager;
+use App\Domain\Services\SymfonyToLegacyHelper;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\DataCollector\AbstractDataCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use function App\await;
 
 class MSPDataCollector extends AbstractDataCollector
 {
@@ -32,5 +37,25 @@ class MSPDataCollector extends AbstractDataCollector
     public function getData(): array
     {
         return $this->data;
+    }
+
+    public function startSimulations(
+        int $session,
+        // below is required by legacy to be auto-wire, has its own ::getInstance()
+        SymfonyToLegacyHelper $helper
+    ): Response {
+        $qb = ConnectionManager::getInstance()->getCachedGameSessionDbConnection($session)->createQueryBuilder();
+        try {
+            $gameState = $qb->select('game_state')->from('game')->fetchOne();
+        } catch (\Exception $e) {
+            return new Response($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        $game = new Game();
+        $game->setGameSessionId($session);
+        if (null === $promise = $game->changeWatchdogState($gameState)) {
+            return new Response('Could not change watchdog state', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        $arrResult = await($promise);
+        return new Response(json_encode($arrResult));
     }
 }
