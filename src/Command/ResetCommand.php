885357392089022cf5d0a1061a21cd1bc90b762d
Harald Warmelink <warmelink.h@buas.nl>
2023-08-22 09:07:08 +0200

    MSP-3290 creates first simple version of reset command, for now only focused on sessions rather than configs, saves or settings

--- src/Command/ResetCommand.php
+++ src/Command/ResetCommand.php
@@ -2,12 +2,17 @@
 
 namespace App\Command;
 
+use App\Domain\Services\ConnectionManager;
 use App\Entity\ServerManager\GameList;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -24,7 +29,8 @@ class ResetCommand extends Command
 
     public function __construct(
         private readonly EntityManagerInterface $mspServerManagerEntityManager,
-        private readonly KernelInterface $kernel
+        private readonly KernelInterface $kernel,
+        private readonly ConnectionManager $connectionManager
     ) {
         parent::__construct();
     }
@@ -41,6 +47,17 @@ class ResetCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+        $sessionRange[0] = 1;
+        $sessionRange[1] = 99;
+        $sessionRangeInput = $input->getArgument('sessions');
+        if ($sessionRangeInput) {
+            $sessionRange = explode('-', $sessionRangeInput);
+            if (count($sessionRange) !== 2) {
+                throw new \Exception('Please specify a sessions range in the format min-max, like this: 1-10');
+            }
+            $io->info("Only removing sessions {$sessionRange[0]} through {$sessionRange[1]}.");
+        }
         $helper = $this->getHelper('question');
         $question = new ConfirmationQuestion(
             'This will reset your Server Manager. Are you sure you want to continue?',
@@ -51,37 +68,45 @@ class ResetCommand extends Command
             return Command::SUCCESS;
         }
 
-        $sessionRange = $input->getArgument('sessions');
-        if ($sessionRange) {
-            $sessionRange = explode('-', $sessionRange);
-            if (count($sessionRange) !== 2) {
-                throw new \Exception('Please specify a sessions range in the format min-max, like this: 1-10');
-            }
-        } else {
-            $sessionRange[0] = 1;
-            $sessionRange[1] = 99;
-        }
         for ($id = $sessionRange[0]; $id <= $sessionRange[1]; $id++) {
             $session = $this->mspServerManagerEntityManager->getRepository(GameList::class)->find($id);
             if (is_null($session)) {
                 continue;
             }
             $filesystem = new Filesystem();
-            $filesystem->remove([
+            $fileArray = [
                 $this->kernel->getProjectDir()."/ServerManager/log/log_session_{$session->getId()}.log",
                 $this->kernel->getProjectDir()."/ServerManager/session_archive/session_archive_{$session->getId()}.zip",
                 $this->kernel->getProjectDir()."/session_archive/session_archive_{$session->getId()}.zip",
                 $this->kernel->getProjectDir()."/raster/{$session->getId()}/",
                 $this->kernel->getProjectDir()."/running_session_config/session_config_{$session->getId()}.json"
-            ]);
-            // todo: delete session database
+            ];
+            $io->info('Removing the following files: '.var_export($fileArray, true));
+            $filesystem->remove($fileArray);
+
+            $this->dropDatabase($session->getId());
+
             $this->mspServerManagerEntityManager->remove($session);
             $this->mspServerManagerEntityManager->flush();
         }
 
-        $io = new SymfonyStyle($input, $output);
+
         $io->success('Server reset complete.');
 
         return Command::SUCCESS;
+    }
+
+    public function dropDatabase($id): void
+    {
+        $conn = $this->connectionManager->getGameSessionDbName($id);
+        $app = new Application($this->kernel);
+        $input = new ArrayInput([
+            'command' => 'doctrine:database:drop',
+            '--force' => true,
+            '--connection' => $conn,
+            '--env' => $_ENV['APP_ENV']
+        ]);
+        $input->setInteractive(false);
+        $app->doRun($input, new NullOutput());
     }
 }
