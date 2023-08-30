42ac95ec808539eb5272dc1706edb7935c508b23
Marin <hekman.m@buas.nl>
2023-08-30 17:34:09 +0200

    fixed code sniffer issues

--- api/v1/Game.php
+++ api/v1/Game.php
@@ -732,7 +732,8 @@ class Game extends Base
         }
         return $promise
             ->then(function () {
-                return GameSession::getRequestApiRootAsync(getenv('DOCKER') !== false); // $apiRoot is passed to watchdog
+                // $apiRoot is passed to watchdog, so if DOCKER env.var is defined we should use http://caddy:80
+                return GameSession::getRequestApiRootAsync(getenv('DOCKER') !== false);
             })
             ->then(function (string $apiRoot) use ($newWatchdogGameState) {
                 $simulationsHelper = new Simulations();
