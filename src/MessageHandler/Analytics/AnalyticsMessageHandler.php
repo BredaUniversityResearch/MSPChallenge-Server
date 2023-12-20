f9580eddf07b22e4b1e6041aeaa63dd4f5c26f3f
Bas <bas123456789bas@hotmail.nl>
2023-12-20 14:23:21 +0100

    Change default GURaaSID parameter for dev environment

--- src/MessageHandler/Analytics/AnalyticsMessageHandler.php
+++ src/MessageHandler/Analytics/AnalyticsMessageHandler.php
@@ -2,10 +2,12 @@
 
 namespace App\MessageHandler\Analytics;
 
+use Exception;
 use App\Message\Analytics\AnalyticsMessageBase;
 use App\MessageTransformer\Analytics\GURaaSMessageTransformer;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\Exception\RecoverableMessageHandlingException;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -41,10 +43,15 @@ class AnalyticsMessageHandler
         }
 
         $result = $this->postRequestToGURaaS($requestBody);
-        //TODO: properly handle result, retry certain amount of times when failing to post.
-        // maybe supported by the message handler system already?
+        if (!$result) {
+            throw new RecoverableMessageHandlingException();
+            //TODO: decide if always wanna try forever on failure to POST.
+        }
     }
 
+    /**
+     * @throws Exception
+     */
     private function postRequestToGURaaS($requestBody) : bool
     {
         if (!Uuid::isValid($this->guraasGameId)) {
@@ -57,13 +64,27 @@ class AnalyticsMessageHandler
         }
 
         $url = "https://grg.service.guraas.com/v1/games/$this->guraasGameId/data";
+
         try {
             $response = $this->httpClient->request('POST', $url, ['json' => $requestBody]);
             $statusCode = $response->getStatusCode();
             return $statusCode == 201;
-        } catch (TransportExceptionInterface $e) {
-            $this->logger->error("POST request to GURaaS failed with exception: " . $e->getMessage());
+        } catch (Exception $e) {
+            $this->logGURaaSPostRequestException($e, $url, $requestBody);
+            throw $e;
         }
-        return false;
+    }
+
+    private function logGURaaSPostRequestException(
+        Exception $exception,
+        $requestUrl,
+        $requestBody,
+    ) {
+        $this->logger->error(
+            "Exception occured during POST request to GURaaS.".
+            "\n Request URI: ". $requestUrl.
+            "\n Request Body: ".json_encode($requestBody).
+            "\n Exception: ".$exception->getMessage()
+        );
     }
 }
