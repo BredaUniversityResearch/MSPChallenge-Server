91570836d995760e85fb2377272fd19c4edb2aad
Harald Warmelink <warmelink.h@buas.nl>
2024-01-16 11:12:52 +0100

    MSP-4892 work in progress

--- src/MessageHandler/GameList/GameListCreationMessageHandler.php
+++ src/MessageHandler/GameList/GameListCreationMessageHandler.php
@@ -40,12 +40,13 @@ class GameListCreationMessageHandler
 
     private GameList $gameSession;
 
-    private Game $game;
+    private array $dataModel;
 
+    /*private Game $game;
     private Layer $layer;
     private Geometry $geometry;
     private Objective $objective;
-    private Plan $plan;
+    private Plan $plan;*/
 
     public function __construct(
         private readonly EntityManagerInterface $mspServerManagerEntityManager,
@@ -56,11 +57,6 @@ class GameListCreationMessageHandler
         private readonly ContainerBagInterface $params,
         private readonly VersionsProvider $provider
     ) {
-        $this->game = new Game();
-        $this->layer = new Layer();
-        $this->geometry = new Geometry();
-        $this->objective = new Objective();
-        $this->plan = new Plan();
     }
 
     /**
@@ -70,17 +66,12 @@ class GameListCreationMessageHandler
     public function __invoke(GameListCreationMessage $gameList): void
     {
         $this->gameSession = $this->gameListRepository->find($gameList->id)
-            or throw new \Exception('Game session not found, so cannot continue.');
-
-        $this->database = ConnectionManager::getInstance()->getGameSessionDbName($this->gameSession->getId());
-        $this->connection = ConnectionManager::getInstance()
-            ->getCachedGameSessionDbConnection($this->gameSession->getId());
-        $this->game->setGameSessionId($this->gameSession->getId());
-        $this->layer->setGameSessionId($this->gameSession->getId());
-        $this->geometry->setGameSessionId($this->gameSession->getId());
-        $this->objective->setGameSessionId($this->gameSession->getId());
-        $this->plan->setGameSessionId($this->gameSession->getId());
+            ?? throw new \Exception('Game session not found, so cannot continue.');
+        $connectionManager = ConnectionManager::getInstance();
+        $this->database = $connectionManager->getGameSessionDbName($this->gameSession->getId());
+        $this->connection = $connectionManager->getCachedGameSessionDbConnection($this->gameSession->getId());
         try {
+            $this->validateGameConfigComplete();
             $this->gameSessionLogger->notice(
                 'Session {name} creation initiated. This might take a while.',
                 ['name' => $this->gameSession->getName(), 'gameSession' => $this->gameSession->getId()]
@@ -96,7 +87,7 @@ class GameListCreationMessageHandler
                 ['name' => $this->gameSession->getName(), 'gameSession' => $this->gameSession->getId()]
             );
             $state = 'healthy';
-        } catch (\Exception $e) {
+        } catch (\Throwable $e) {
             $this->gameSessionLogger->error(
                 'Session {name} failed to create. Try to resolve the problem and retry. Problem: {problem}',
                 [
@@ -198,7 +189,7 @@ class GameListCreationMessageHandler
         $sessionConfigStore = $_ENV['APP_ENV'] == 'test' ?
             $this->params->get('app.session_config_dir_test') :
             $this->params->get('app.session_config_dir');
-        $sessionConfigStore .= "session_config_{$this->gameSession->getId()}.json";
+        $sessionConfigStore .= sprintf($this->params->get('app.session_config_name'), $this->gameSession->getId());
         $fileSystem = new Filesystem();
         $fileSystem->copy(
             $this->params->get('app.server_manager_config_dir').
@@ -233,9 +224,19 @@ class GameListCreationMessageHandler
      */
     private function setupGame(): void
     {
-        $dataModel = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']
-            ?? throw new \Exception('Could not get config file datamodel contents.');
-        $this->game->setupGame($dataModel);
+        $eraRealtimeString = str_repeat(($this->dataModel['era_planning_realtime'] ?? 300). ",", 4);
+        $eraRealtimeString = substr($eraRealtimeString, 0, -1);
+        $this->insert('game', [
+                'game_start' => $this->dataModel['start'] ?? date('Y'),
+                'game_planning_gametime' => $this->dataModel['era_planning_months'] ?? 1,
+                'game_planning_realtime' => $this->dataModel['era_planning_realtime'] ?? 300,
+                'game_planning_era_realtime' => $eraRealtimeString,
+                'game_eratime' =>
+                    max($this->dataModel['era_total_months'], $this->params->get('app.min_game_era_time')),
+                'game_configfile' =>
+                    sprintf($this->params->get('app.session_config_name'), $this->gameSession->getId()),
+                'game_autosave_month_interval' => $this->params->get('app.game_auto_save_interval')
+        ]);
         $this->gameSessionLogger->info(
             'Basic game parameters set up.',
             ['gameSession' => $this->gameSession->getId()]
@@ -247,9 +248,34 @@ class GameListCreationMessageHandler
      */
     private function setupGameCountries(): void
     {
-        $dataModel = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']
-            ?? throw new \Exception('Could not get config file datamodel contents.');
-        $this->game->setupGameCountries($dataModel);
+        $countries = [
+            [
+                'country_id' => 1,
+                'country_colour' => $this->dataModel['user_admin_color'] ?? '#FF00FFFF',
+                'country_is_manager' => 1
+            ],
+            [
+                'country_id' => 2,
+                'country_colour' => $this->dataModel['user_region_manager_color'] ?? '#00FFFFFF',
+                'country_is_manager' => 1
+            ]
+        ];
+        foreach ($this->dataModel['meta'] as $layerMeta) {
+            if ($layerMeta['layer_name'] == $this->dataModel['countries']) {
+                foreach ($layerMeta['layer_type'] as $country) {
+                    $countries[] = [
+                        'country_id' => $country['value'],
+                        'country_name' => $country['displayName'],
+                        'country_colour' => $country['polygonColor'],
+                        'country_is_manager' => 0
+                    ];
+                }
+                break;
+            }
+        }
+        foreach ($countries as $country) {
+            $this->insert('game', $country);
+        }
         $this->gameSessionLogger->info(
             'All countries set up.',
             ['gameSession' => $this->gameSession->getId()]
@@ -262,14 +288,12 @@ class GameListCreationMessageHandler
      */
     private function importLayerData(): void
     {
-        $dataModel = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']
-            ?? throw new \Exception('Could not get config file datamodel contents.');
         $geoServerCommunicator = new GeoServerCommunicator($this->client);
         $geoServerCommunicator->setBaseURL($this->gameSession->getGameGeoServer()->getAddress());
         $geoServerCommunicator->setUsername($this->gameSession->getGameGeoServer()->getUsername());
         $geoServerCommunicator->setPassword($this->gameSession->getGameGeoServer()->getPassword());
 
-        foreach ($dataModel['meta'] as $layerMetaData) {
+        foreach ($this->dataModel['meta'] as $layerMetaData) {
             $this->gameSessionLogger->info(
                 'Starting import of layer {layerName}...',
                 ['gameSession' => $this->gameSession->getId(), 'layerName' => $layerMetaData['layer_name']]
@@ -307,8 +331,6 @@ class GameListCreationMessageHandler
         array $layerMetaData,
         GeoServerCommunicator $geoServerCommunicator
     ): void {
-        $region = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']['region']
-            ?? throw new \Exception('Could not get config file datamodel > region contents.');
         $rasterFileName = $_ENV['APP_ENV'] == 'test' ?
             $this->params->get('app.session_raster_dir_test') :
             $this->params->get('app.session_raster_dir');
@@ -317,21 +339,18 @@ class GameListCreationMessageHandler
             $layerMetaData['layer_download_from_geoserver']
         ) {
             $rasterMetaData = $geoServerCommunicator->getRasterMetaData(
-                $region,
+                $this->dataModel['region'],
                 $layerMetaData['layer_name']
             );
-            $qb = $this->connection->createQueryBuilder();
-            $qb->insert('layer')
-                ->values([
-                    'layer_name' => $qb->createPositionalParameter($layerMetaData['layer_name']),
-                    'layer_geotype' => $qb->createPositionalParameter($layerMetaData['layer_geotype']),
-                    'layer_group' => $qb->createPositionalParameter($region),
-                    'layer_editable' => $qb->createPositionalParameter(0),
-                    'layer_raster' => $qb->createPositionalParameter($rasterMetaData)
-                ])
-                ->executeStatement();
+            $this->insert('layer', [
+                'layer_name' => $layerMetaData['layer_name'],
+                'layer_geotype' => $layerMetaData['layer_geotype'],
+                'layer_group' => $this->dataModel['region'],
+                'layer_editable' => 0,
+                'layer_raster' => $rasterMetaData
+            ]);
             $rasterData = $geoServerCommunicator->getRasterDataThroughMetaData(
-                $region,
+                $this->dataModel['region'],
                 $layerMetaData,
                 $rasterMetaData
             );
@@ -351,17 +370,12 @@ class GameListCreationMessageHandler
             return;
         }
         // Create the metadata for the raster layer, but don't fill in the layer_raster field.
-        $qb = $this->connection->createQueryBuilder();
-        $qb->insert('layer')
-            ->values(
-                [
-                    'layer_name' => $qb->createPositionalParameter($layerMetaData['layer_name']),
-                    'layer_geotype' => $qb->createPositionalParameter($layerMetaData['layer_geotype']),
-                    'layer_group' => $qb->createPositionalParameter($region),
-                    'layer_editable' => $qb->createPositionalParameter(0)
-                ]
-            )
-            ->executeStatement();
+        $this->insert('layer', [
+            'layer_name' => $layerMetaData['layer_name'],
+            'layer_geotype' => $layerMetaData['layer_geotype'],
+            'layer_group' => $this->dataModel['region'],
+            'layer_editable' => 0
+        ]);
         $this->gameSessionLogger->info(
             'Successfully retrieved {layerName} without storing a raster file, as requested.',
             [
@@ -380,17 +394,28 @@ class GameListCreationMessageHandler
         array $layerMetaData,
         GeoServerCommunicator $geoServerCommunicator
     ): void {
-        $region = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']['region']
-            ?? throw new \Exception('Could not get config file datamodel > region contents.');
         if (!array_key_exists('layer_download_from_geoserver', $layerMetaData) ||
             $layerMetaData['layer_download_from_geoserver']
         ) {
-            $layerId = $this->layer->getIdOrAdd([
-                'layer_name' => $layerMetaData['layer_name'],
-                'layer_geotype' => $layerMetaData['layer_geotype'],
-                'layer_group' => $region
-            ]);
-            $layersContainer = $geoServerCommunicator->getLayerDescription($region, $layerMetaData['layer_name']);
+            $qb = $this->connection->createQueryBuilder();
+            $result = $qb->select('l.layer_id')
+                ->from('layer', 'l')
+                ->where($qb->expr()->eq('l.layer_name', $layerMetaData['layer_name']))
+                ->executeQuery()
+                ->fetchOne();
+            if ($result === false) {
+                $layerId = $this->insert('layer', [
+                    'layer_name' => $layerMetaData['layer_name'],
+                    'layer_geotype' => $layerMetaData['layer_geotype'],
+                    'layer_group' => $this->dataModel['region']
+                ]);
+            } else {
+                $layerId = $result['layer_id'];
+            }
+            $layersContainer = $geoServerCommunicator->getLayerDescription(
+                $this->dataModel['region'],
+                $layerMetaData['layer_name']
+            );
             foreach ($layersContainer as $layerWithin) {
                 $geometryData = $geoServerCommunicator->getLayerGeometry($layerWithin['layerName']);
                 $features = $geometryData['features']
@@ -441,16 +466,11 @@ class GameListCreationMessageHandler
             return;
         }
         // Create the metadata for the vector layer, but don't fill the geometry table.
-        $qb = $this->connection->createQueryBuilder();
-        $qb->insert('layer')
-            ->values(
-                [
-                    'layer_name' => $qb->createPositionalParameter($layerMetaData['layer_name']),
-                    'layer_geotype' => $qb->createPositionalParameter($layerMetaData['layer_geotype']),
-                    'layer_group' => $qb->createPositionalParameter($region)
-                ]
-            )
-            ->executeStatement();
+        $this->insert('layer', [
+            'layer_name' => $layerMetaData['layer_name'],
+            'layer_geotype' => $layerMetaData['layer_geotype'],
+            'layer_group' => $this->dataModel['region']
+        ]);
         $this->gameSessionLogger->info(
             'Successfully retrieved {layerName} without storing geometry in the database, as requested.',
             ['gameSession' => $this->gameSession->getId(), 'layerName' => $layerMetaData['layer_name']]
@@ -462,15 +482,7 @@ class GameListCreationMessageHandler
      */
     private function setupLayerMeta(): void
     {
-        $layers = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']['meta']
-            ?? throw new \Exception('Could not get config file datamodel > meta contents.');
-        if (empty($layers)) {
-            $this->gameSessionLogger->info(
-                'No layer metadata to set up.',
-                ['gameSession' => $this->gameSession->getId()]
-            );
-        }
-        foreach ($layers as $layerMetaData) {
+        foreach ($this->dataModel['meta'] as $layerMetaData) {
             $this->layer->setupMetaForLayer($layerMetaData);
         }
         $this->gameSessionLogger->info(
@@ -484,15 +496,13 @@ class GameListCreationMessageHandler
      */
     private function setupRestrictions(): void
     {
-        $dataModel = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']
-            ?? throw new \Exception('Could not get config file datamodel contents.');
-        if (empty($dataModel['restrictions'])) {
+        if (empty($this->dataModel['restrictions'])) {
             $this->gameSessionLogger->info(
                 'No layer restrictions to set up.',
                 ['gameSession' => $this->gameSession->getId()]
             );
         }
-        $setupReturn = $this->plan->setupRestrictions($dataModel);
+        $setupReturn = $this->plan->setupRestrictions($this->dataModel);
         if (is_array($setupReturn)) {
             foreach ($setupReturn as $returnedWarning) {
                 $this->gameSessionLogger->warning(
@@ -509,14 +519,12 @@ class GameListCreationMessageHandler
 
     private function setupSimulations(): void
     {
-        $dataModel = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']
-            ?? throw new \Exception('Could not get config file datamodel contents.');
         $nameSpaceName = (new \ReflectionClass(Simulations::class))->getNamespaceName();
         $simulationsDone = [];
         $possibleSims = array_keys($this->provider->getComponentsVersions());
         foreach ($possibleSims as $possibleSim) {
-            if (array_key_exists($possibleSim, $dataModel)
-                && is_array($dataModel[$possibleSim])
+            if (array_key_exists($possibleSim, $this->dataModel)
+                && is_array($this->dataModel[$possibleSim])
                 && class_exists("{$nameSpaceName}\\{$possibleSim}")
                 && method_exists("{$nameSpaceName}\\{$possibleSim}", 'onSessionSetup')
                 && method_exists("{$nameSpaceName}\\{$possibleSim}", 'setGameSessionId')
@@ -527,7 +535,7 @@ class GameListCreationMessageHandler
                 );
                 $simulation = new ("{$nameSpaceName}\\{$possibleSim}")();
                 $simulation->setGameSessionId($this->gameSession->getId());
-                $return = $simulation->onSessionSetup($dataModel);
+                $return = $simulation->onSessionSetup($this->dataModel);
                 if (is_array($return)) {
                     foreach ($return as $message) {
                         $this->gameSessionLogger->warning(
@@ -549,7 +557,7 @@ class GameListCreationMessageHandler
         }
         $remainingSims = array_diff($possibleSims, $simulationsDone);
         foreach ($remainingSims as $remainingSim) {
-            if (key_exists($remainingSim, $dataModel)) {
+            if (key_exists($remainingSim, $this->dataModel)) {
                 $this->gameSessionLogger->error(
                     'Unable to set up {simulation}.',
                     ['simulation' => $remainingSim, 'gameSession' => $this->gameSession->getId()]
@@ -563,9 +571,7 @@ class GameListCreationMessageHandler
      */
     private function setupPlans(): void
     {
-        $dataModel = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']
-            ?? throw new \Exception('Could not get config file datamodel contents.');
-        $return = $this->plan->setupPlans($dataModel);
+        $return = $this->plan->setupPlans($this->dataModel);
         if (is_array($return)) {
             foreach ($return as $message) {
                 $this->gameSessionLogger->warning(
@@ -583,9 +589,7 @@ class GameListCreationMessageHandler
 
     private function setupObjectives(): void
     {
-        $dataModel = $this->gameSession->getGameConfigVersion()->getGameConfigComplete()['datamodel']
-            ?? throw new \Exception('Could not get config file datamodel contents.');
-        $this->objective->setupObjectives($dataModel);
+        $this->objective->setupObjectives($this->dataModel);
     }
 
     /**
@@ -628,5 +632,30 @@ class GameListCreationMessageHandler
                 ['gameSession' => $this->gameSession->getId()]
             );
         }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function validateGameConfigComplete(): void
+    {
+        $completeConfig = $this->gameSession->getGameConfigVersion()->getGameConfigComplete();
+        // https://github.com/justinrainbow/json-schema !!
+        $this->dataModel = $completeConfig['datamodel'] ?? throw new \Exception('nope.');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function insert(string $table, array $values): int
+    {
+        $qb = $this->connection->createQueryBuilder();
+        foreach ($values as $key => $value) {
+            $values[$key] = $qb->createPositionalParameter($value);
+        }
+        $qb->insert($table)
+            ->values($values)
+            ->executeStatement();
+        return $this->connection->lastInsertId('auto_increment');
     }
 }
