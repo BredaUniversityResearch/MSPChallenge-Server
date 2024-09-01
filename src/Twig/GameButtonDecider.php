0d6d21d625110921e3fcfb4d385f4e116fa3d6b2
Harald <h.j.g.warmelink@gmail.com>
2024-09-01 16:13:34 +0200

    more work on game buttons

--- src/Twig/GameButtonDecider.php
+++ src/Twig/GameButtonDecider.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Twig;
 
+use App\Domain\Common\EntityEnums\GameSessionStateValue;
+use App\Domain\Common\EntityEnums\GameStateValue;
 use App\Entity\ServerManager\GameList;
 use Doctrine\ORM\EntityManagerInterface;
 use Twig\Extension\AbstractExtension;
@@ -26,12 +28,36 @@ class GameButtonDecider extends AbstractExtension
             $gameSession = $this->mspServerManagerEntityManager->getRepository(GameList::class)
                 ->createGameListFromData($gameSession);
         }
-        if ($buttonType == 'play') {
-            if ($gameSession->getSessionState() == 'healthy'
-                && $gameSession->getGameState() == 'pause'
-            ) {
-                return true;
-            }
+        switch ($buttonType) {
+            case 'recreate':
+                if (!$gameSession->getSessionState() == GameSessionStateValue::ARCHIVED) {
+                    return true;
+                }
+                break;
+            case 'play':
+                if ($gameSession->getSessionState() == GameSessionStateValue::HEALTHY && $gameSession->getGameState() == GameStateValue::PAUSE) {
+                    return true;
+                }
+                break;
+            case 'pause':
+                if ($gameSession->getSessionState() == GameSessionStateValue::HEALTHY 
+                    && ($gameSession->getGameState() == GameStateValue::PLAY
+                        || $gameSession->getGameState() == GameStateValue::FASTFORWARD
+                        || $gameSession->getGameState() == GameStateValue::SETUP))
+                {
+                    return true;
+                }
+                break;
+            case 'save':
+            case 'archive':
+                if ($gameSession->getSessionState() == GameSessionStateValue::HEALTHY 
+                    && ($gameSession->getGameState() == GameStateValue::PAUSE
+                        || $gameSession->getGameState() == GameStateValue::SETUP))
+                {
+                    return true;
+                }
+                break;
         }
+        return false;
     }
 }
