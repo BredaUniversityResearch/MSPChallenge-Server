32197c54a85e56986edd2209fe33dcac917c29b4
Harald <h.j.g.warmelink@gmail.com>
2024-09-22 23:07:32 +0200

    next steps in game user access modal

--- assets/controllers/modal-game-user-access_controller.js
+++ assets/controllers/modal-game-user-access_controller.js
@@ -8,34 +8,34 @@ export default class extends Controller {
     passwordRegion;
     passwordPlayer;
 
-    connect()
+    connect(event)
     {
         this.passwordAdmin = JSON.parse($('#game_list_user_access_form_passwordAdmin').val()).admin;
         this.passwordRegion = JSON.parse($('#game_list_user_access_form_passwordAdmin').val()).region;
         this.passwordPlayer = JSON.parse($('#game_list_user_access_form_passwordPlayer').val());
-
-        this.setupUserAccessFormInitialState();
+        this.setupInitialState(event);
     }
 
-    setupUserAccessFormInitialState()
-    {
-        this.setupPasswordOrUserVisibilities();
-    }
-
-    setupPasswordOrUserVisibilities()
+    setupInitialState(event)
     {
         if (this.passwordAdmin.provider == 'local') {
             $('#adminUserFields').hide();
-            $('#game_list_user_access_form_password_admin').val(this.passwordAdmin.value)
+            $('#game_list_user_access_form_password_admin').val(this.passwordAdmin.value);
         } else {
             $('#adminPasswordFields').hide();
+            $('#game_list_user_access_form_provider_admin').val(this.passwordAdmin.provider);
+            this.addInitialUserInput(event, this.passwordAdmin.value, 'usersAdmin');
         }
+
         if (this.passwordRegion.provider == 'local') {
             $('#regionUserFields').hide();
-            $('#game_list_user_access_form_password_region').val(this.passwordRegion.value)
+            $('#game_list_user_access_form_password_region').val(this.passwordRegion.value);
         } else {
             $('#regionPasswordFields').hide();
+            $('#game_list_user_access_form_provider_region').val(this.passwordRegion.provider);
+            this.addInitialUserInput(event, this.passwordRegion.value, 'usersRegion');
         }
+
         if (this.passwordPlayer.provider == 'local') {
             $('#playerUserFields').hide();
         } else {
@@ -56,12 +56,23 @@ export default class extends Controller {
         }
     }
 
-    addUserInput(event, content = '')
+    addInitialUserInput(event, passwordTypeValue, userType)
     {
-        event.preventDefault();
-        let target = $(`#${event.currentTarget.dataset.collection}`);
+        let passwordTypeValueArray = passwordTypeValue.split('|');
+        for (const user of passwordTypeValueArray) {
+            this.addUserInput(event, userType, user);
+        }
+    }
+
+    addUserInput(event, targetOverride = '', content = '')
+    {
+        if (targetOverride) {
+            var target = $(`#${targetOverride}`);
+        } else {
+            var target = $(`#${event.currentTarget.dataset.collection}`);
+        }
         let inputToAdd = target.data('prototype').replace(/__name__/g, 0);
-        inputToAdd = inputToAdd.replace(/value=""/g, `value="${content}"`);
+        inputToAdd = inputToAdd.replace(/type="text"/g, `type="text" value="${content}"`);
         target.before(inputToAdd);
     }
     
