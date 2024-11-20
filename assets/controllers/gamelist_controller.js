aaff1dedd8a7dd79a7f02ade0a77c49bb564753a
Harald <h.j.g.warmelink@gmail.com>
2024-11-20 10:02:52 +0100

    adds filter options to Saves list

--- assets/controllers/gamelist_controller.js
+++ assets/controllers/gamelist_controller.js
@@ -5,22 +5,10 @@ export default class extends Controller {
 
     connect()
     {
-        let frame = this.getFrame();
+        let frame = document.querySelector('turbo-frame#sessionsTable');
         setInterval(function () {
             frame.reload();
         }, 10000);
-    }
-
-    applyFilter(event)
-    {
-        let frame = this.getFrame();
-        frame.src = event.currentTarget.dataset.href;
-        frame.reload();
-    }
-
-    getFrame()
-    {
-        return document.querySelector('turbo-frame#sessionsTable');
     }
 
 }
