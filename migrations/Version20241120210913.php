ba167e28090a02a4a38cee4be4bec3d9772c232b
Marin <hekman.m@buas.nl>
2024-11-21 10:39:35 +0100

    setup the migration for "Insert new config for Eastern med Sea" a bit more flexible

--- migrations/Version20241120210913.php
+++ migrations/Version20241120210913.php
@@ -22,25 +22,32 @@ final class Version20241120210913 extends MSPMigration
     {
         // phpcs:ignoreFile Generic.Files.LineLength.TooLong
         $this->addSql(<<<'SQL'
-INSERT INTO `game_config_files` (`id`, `filename`, `description`) VALUES
-(7, 'Eastern_Med_Sea_basic', 'Eastern Mediterranean Sea basic configuration file supplied by BUas')
+INSERT INTO `game_config_files` (`filename`, `description`) VALUES
+('Eastern_Med_Sea_basic', 'Eastern Mediterranean Sea basic configuration file supplied by BUas')
 SQL
         );
+        $lastInsertedId = $this->connection->lastInsertId();
         $this->addSql(<<<'SQL'
-INSERT INTO `game_config_version` (`id`, `game_config_files_id`, `version`, `version_message`, `visibility`, `upload_time`, `upload_user`, `last_played_time`, `file_path`, `region`, `client_versions`) VALUES
-(7, 7, 1, 'See www.mspchallenge.info', 'active', unix_timestamp(), 1, 0, 'Eastern_Med_Sea_basic/Eastern_Med_Sea_basic_1.json', 'easternmed', 'Any')
+INSERT INTO `game_config_version` (`game_config_files_id`, `version`, `version_message`, `visibility`, `upload_time`, `upload_user`, `last_played_time`, `file_path`, `region`, `client_versions`) VALUES
+($lastInsertedId, 1, 'See www.mspchallenge.info', 'active', unix_timestamp(), 1, 0, 'Eastern_Med_Sea_basic/Eastern_Med_Sea_basic_1.json', 'easternmed', 'Any')
 SQL
         );
     }
 
     protected function onDown(Schema $schema): void
     {
+        // Delete dependent records in game_list
         $this->addSql(<<<'SQL'
-DELETE FROM `game_config_version` WHERE `id`=7
+DELETE FROM `game_list` WHERE `game_config_version_id` IN (SELECT id FROM `game_config_version` WHERE `game_config_files_id` IN (SELECT id FROM `game_config_files` WHERE `filename` = 'Eastern_Med_Sea_basic'))
+SQL
+        );
+
+        $this->addSql(<<<'SQL'
+DELETE FROM `game_config_version` WHERE `game_config_files_id` IN (SELECT id FROM `game_config_files` WHERE `filename` = 'Eastern_Med_Sea_basic')
 SQL
         );
         $this->addSql(<<<'SQL'
-DELETE FROM `game_config_files` WHERE `id`=7
+DELETE FROM `game_config_files` WHERE `filename` = 'Eastern_Med_Sea_basic'
 SQL
         );
     }
