f90c415891c2b69c47eb95cd949f943ed4daedb7
Marin <89514754+marinhekman@users.noreply.github.com>
2025-03-25 21:13:49 +0100

    * suppress output from "php docker/export-dotenv-vars/app.php" (#353)
    * disabling verbose mode for doctrine migrations in docker-entrypoint.sh
    * added button to connect watchdog to session
    * added show logs button showing connection issues for a watchdog

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -60,7 +60,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		fi
 
 		if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
-			php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing -vvv
+			php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing # -vvv for very verbose mode
 		fi
 	fi
 
