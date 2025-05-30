ac5a0a9327f84fe8b101c4b5c448189629c34b44
Marin <hekman.m@buas.nl>
2025-05-30 10:01:49 +0200

    lets not use setfacl depending on acl. I had a "setfacl operation not supported"

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -2,23 +2,6 @@
 set -e
 
 if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-  if [ -d "config" ]; then
-    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX config
-    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX config
-  fi
-  if [ -f "composer.json" ]; then
-    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX composer.json
-    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX composer.json
-  fi
-  if [ -f "composer.lock" ]; then
-    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX composer.lock
-    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX composer.lock
-  fi
-  if [ -f "symfony.lock" ]; then
-    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX symfony.lock
-    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX symfony.lock
-  fi
-
 #	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
 #		composer install --prefer-dist --no-progress --no-interaction
 #	fi
@@ -27,15 +10,6 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
   # Display information about the current project
 	# Or about an error in project initialization
 	php bin/console -V
-
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX config
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX config
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX composer.json
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX composer.json
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX composer.lock
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX composer.lock
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX symfony.lock
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX symfony.lock
 
 	if grep -q ^DATABASE_URL= .env; then
 		echo 'Waiting for database to be ready...'
@@ -64,26 +38,8 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		fi
 	fi
 
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX export
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX export
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX raster
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX raster
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX running_session_config
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX running_session_config
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/configfiles
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/configfiles
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/log
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/log
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/saves
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/saves
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/session_archive
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX ServerManager/session_archive
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX session_archive
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX session_archive
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
-  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX POV
-  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX POV
+  chown -R www-data:www-data export raster running_session_config ServerManager/configfiles ServerManager/log ServerManager/saves ServerManager/session_archive session_archive var vendor POV
+  chmod -R u+rwX,g+rwX,o+rX export raster running_session_config ServerManager/configfiles ServerManager/log ServerManager/saves ServerManager/session_archive session_archive var vendor POV
 
   echo 'PHP app ready!'
 
