76e529e3bf7ba8d45a4bfe65183791d090ec6738
Webmaster@Cradle <92438529+CradleWebmaster@users.noreply.github.com>
2025-06-03 00:42:11 +0200

    Setup wizard (base on msp-ar) (#382)
    * Revert "Revert "some updates""
    
    This reverts commit b501fe10ababb0e7675831a61e2c0a365322c63d.
    
    * Revert "Revert "expose docker api""
    
    This reverts commit 6562e79f5efde27a203e2b17db3878ec012ad8df.
    
    * Revert "Revert "we are *not* gonna expose it here""
    
    This reverts commit 45f4ea8ebfe1ea110d0fa9e7510b7322e841c02a.
    
    * Revert "Revert "add docker and docker compose . we will need it to create other conainers later""
    
    This reverts commit aa79bd53a5bf64d396f2c86b05cce4af218a9861.
    
    * only one network needed
    
    * update comments in Dockerfile
    
    * setup scripts
    
    * using .env as well
    
    * no need for docker calls inside php container
    
    * new lines
    
    ---------
    
    Co-authored-by: Marin <hekman.m@buas.nl>

---
+++ setup/staging.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BRANCH_NAME="dev"
+if [ $# -gt 0 ]; then
+  BRANCH_NAME="$1"
+fi
+
+curl -O "https://raw.githubusercontent.com/BredaUniversityResearch/MSPChallenge-Server/refs/heads/${BRANCH_NAME}/docker-compose.yml"
+curl -O "https://raw.githubusercontent.com/BredaUniversityResearch/MSPChallenge-Server/refs/heads/${BRANCH_NAME}/docker-compose.staging.yml"
+
+# Check if .env exists and copy it to .env.local
+if [ -f ".env" ]; then
+  cp .env .env.local
+fi
+
+# Load environment variables from .env.local if it exists
+if [ -f ".env.local" ]; then
+  source .env.local
+fi
+
+if [ -z "${CADDY_MERCURE_JWT_SECRET}" ]; then
+  CADDY_MERCURE_JWT_SECRET=$(echo $RANDOM | md5sum | head -c 32)
+fi
+
+# Append variables to .env.local
+cat <<EOF > .env.local
+CADDY_MERCURE_JWT_SECRET=${CADDY_MERCURE_JWT_SECRET}
+EOF
+
+docker compose --env-file .env.local -f docker-compose.yml -f "docker-compose.staging.yml" up -d
