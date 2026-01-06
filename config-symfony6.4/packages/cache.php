776487d3f8b5b6bda293ac199537015d5af3cddc
Marin <hekman.m@buas.nl>
2026-01-06 23:46:12 +0100

    removed symfony 6.4 config

--- config-symfony6.4/packages/cache.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Symfony\Config\FrameworkConfig;
-
-return static function (FrameworkConfig $frameworkConfig): void {
-    $frameworkConfig->cache()
-        ->app($_ENV['FRAMEWORK_CACHE_APP'] ?? 'cache.adapter.filesystem');
-};
