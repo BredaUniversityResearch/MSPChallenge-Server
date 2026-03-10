851cb78ffd73a35c3d3afa1b7fbc6a64947bba27
Marin <hekman.m@buas.nl>
2026-03-10 10:27:22 +0100

    always update raster bounds if given

--- api/v1/Layer.php
+++ api/v1/Layer.php
@@ -363,6 +363,16 @@ class Layer extends Base
         $curMonth = $game->GetCurrentMonthAsId();
         $month ??= $curMonth;
 
+        // raster bounds update
+        $rasterDataUpdated = false;
+        if (!empty($raster_bounds)) {
+            $this->log('Set raster for layer '.$layer->getLayerName());
+            $layer
+                ->setLayerLastupdate(microtime(true))
+                ->setLayerRaster($rasterData->setBoundingbox($raster_bounds));
+            $rasterDataUpdated = true;
+        }
+
         // we are processing the next month, so:
         // - replace the current one in raster/ folder,
         // - allow raster bounds updates
@@ -381,24 +391,16 @@ class Layer extends Base
                 $this->log('Failed to save given image_data to '.$f.':'.$e->getMessage(), self::LOG_LEVEL_ERROR);
             }
 
-            // raster bounds update
-            $rasterDataUpdated = false;
-            if (!empty($raster_bounds)) {
-                $this->log('Set raster for layer '.$layer->getLayerName());
-                $layer->setLayerRaster($rasterData->setBoundingbox($raster_bounds));
-                $rasterDataUpdated = true;
-            }
-
             // update the layer record
             $layer
                 ->setLayerLastupdate(microtime(true))
                 ->setLayerMelupdate(1);
-            $this->log(sprintf(
-                'Updated layer %s with id %d',
-                ($rasterDataUpdated ? ' incl. raster data' : ''),
-                $layer->getLayerId()
-            ));
         }
+        $this->log(sprintf(
+            'Updated layer %s with id %d',
+            ($rasterDataUpdated ? ' incl. raster data' : ''),
+            $layer->getLayerId()
+        ));
 
         // (Pre-)archive the raster file
         Store::EnsureFolderExists(Store::GetRasterArchiveFolder($this->getGameSessionId()));
